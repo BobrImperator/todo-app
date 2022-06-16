2470863f817b8dc4cf1d599283fd1a3f04dd9020
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2022-06-16 19:58:35 +0200

    feat: change how moving items between lists work

--- todo-app.js
+++ todo-app.js
@@ -1,7 +1,24 @@
+const ALL_TODOS_ELEMENT = "[element-all-todos]";
+const DONE_TODOS_ELEMENT = "[element-done-todos]";
+
 class TodoItem {
   name;
   isDone = false;
 }
+
+const clearList = (selector) => {
+  let list = document.querySelector(selector);
+
+  while (list.lastChild) {
+    list.removeChild(list.lastChild);
+  }
+};
+
+const renderTodo = function todoRenderer(list, item) {
+  let li = document.createElement("li");
+  li.innerText = item.name;
+  list.appendChild(li);
+};
 
 class TodoApp {
   todos = [];
@@ -9,32 +26,33 @@ class TodoApp {
   constructor() {
     this.renderTodos();
     this.formComponent();
-    this.registerClick("[element-all-todos]", "[element-done-todos]");
-    this.registerClick("[element-done-todos]", "[element-all-todos]");
+    this.registerClick(ALL_TODOS_ELEMENT, DONE_TODOS_ELEMENT);
+    this.registerClick(DONE_TODOS_ELEMENT, ALL_TODOS_ELEMENT);
   }
 
   renderTodos() {
-    let list = document.querySelector("[element-all-todos]");
-
-    while (list.lastChild) {
-      list.removeChild(list.lastChild);
-    }
+    clearList(ALL_TODOS_ELEMENT);
+    clearList(DONE_TODOS_ELEMENT);
+    let allTodoList = document.querySelector(ALL_TODOS_ELEMENT);
+    let doneTodoList = document.querySelector(DONE_TODOS_ELEMENT);
 
     for (let item of this.todos) {
-      let li = document.createElement("li");
-      li.innerText = item.name;
-      list.appendChild(li);
+      if (item.isDone) {
+        renderTodo(doneTodoList, item);
+      } else {
+        renderTodo(allTodoList, item);
+      }
     }
   }
 
   formComponent() {
     const todoForm = document.querySelector("[element-form]");
-    
+
     todoForm.addEventListener("submit", (event) => {
       event.preventDefault();
 
       const formData = new FormData(event.target);
-     
+
       let newTodo = new TodoItem();
       newTodo.name = formData.get("name");
       let isDone = formData.get("isDone");
@@ -51,19 +69,29 @@ class TodoApp {
     let list = document.querySelector(sourceElement);
     let targetList = document.querySelector(targetElement);
 
-    list.addEventListener('click', (event) => {
+    list.addEventListener("click", (event) => {
       let child = event.srcElement;
-      targetList.appendChild(child);
+      
+      let todo = this.todos.find((todo) => todo.name === child.innerText);
+      todo.isDone = !todo.isDone;
+      
+      this.renderTodos();
+      console.log(this.todos);
+      // targetList.appendChild(child);
       // Mutable https://developer.mozilla.org/en-US/docs/Glossary/Mutable
       // let index = this.todos.findIndex((todo) => todo.name === child.innerText);
       // this.todos.splice(index, 1);
 
       // Immutable https://developer.mozilla.org/en-US/docs/Glossary/Immutable
-      this.todos = this.todos.filter((todo) => todo.name !== child.innerText);
+      // this.todos = this.todos.filter((todo) => todo.name !== child.innerText);
     });
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  new TodoApp();
-}, { once: true });
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    new TodoApp();
+  },
+  { once: true }
+);
