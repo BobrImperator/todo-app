34b9a268b8c340439c834d02514f66d0f6c1f7db
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2022-07-18 19:59:59 +0200

    Lesson 8 (#9)
    * feat: create simple todos server
    
    * feat: read todos from server
    
    * refactor: use async/await
    
    * feat: create new todos
    
    * feat: for fun camelize, snakelize helper functions

--- native-frontend/todo-app.js
+++ native-frontend/todo-app.js
@@ -17,15 +17,91 @@ const clearList = (selector) => {
   }
 };
 
+// INPUT { isDone: true, name: "Ree" }
+// OUTPUT { is_done: true, name: "Ree" }
+const CAPITAL_LETTERS_REGEX = /(?=[A-Z])/g;
+
+const snakelize = (data) => {
+  return Object.fromEntries(
+    Object.entries(data).map((entry) => {
+      let match = entry[0].split(CAPITAL_LETTERS_REGEX);
+
+      let newEntry;
+
+      if (!match) {
+        newEntry = entry[0];
+      } else {
+        newEntry = match.map((word) => word.toLowerCase()).join("_");
+      }
+
+      return [
+        newEntry,
+        entry[1], // value
+      ];
+    })
+  );
+};
+
+const camelize = (data) => {
+  return Object.fromEntries(
+    Object.entries(data).map((entry) => {
+      let match = entry[0].split("_");
+
+      let newEntry;
+
+      if (!match) {
+        newEntry = entry[0];
+      } else {
+        let [first, ...others] = match;
+
+        if (others.length) {
+          let o = others.map((word) => {
+            let [firstLetter, ...letters]= word.split("");
+            
+            return [firstLetter.toUpperCase(), ...letters].join('');
+          });
+
+          newEntry = `${match[0]}${o}`;
+        } else {
+          newEntry = entry[0];
+        }
+      }
+
+      return [
+        newEntry,
+        entry[1], // value
+      ];
+    })
+  );
+};
+
 class TodoApp {
   todos;
 
   constructor() {
-    this.todos = getTodos();
-    console.log(this.todos);
-    this.renderTodos();
-    this.formComponent();
-    this.clearTodosComponent();
+    this.start();
+
+    let todo = new TodoItem();
+
+    let snake = snakelize(todo);
+
+    let camel = camelize(snake);
+    console.log(snake);
+    console.log(camel);
+  }
+
+  async start() {
+    try {
+      let response = await fetch("http://localhost:8080/todos");
+      let todos = await response.json();
+
+      this.todos = todos;
+      this.renderTodos();
+      this.formComponent();
+      this.clearTodosComponent();
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   renderTodos() {
@@ -46,7 +122,7 @@ class TodoApp {
   formComponent() {
     const todoForm = document.querySelector("[element-form]");
 
-    todoForm.addEventListener("submit", (event) => {
+    todoForm.addEventListener("submit", async (event) => {
       event.preventDefault();
 
       const formData = new FormData(event.target);
@@ -56,13 +132,22 @@ class TodoApp {
       let isDone = formData.get("isDone");
       newTodo.isDone = Boolean(isDone);
 
-      let todoIds = this.todos.map((todo) => todo.id);
-      let maxId = Math.max(...todoIds);
+      let response = await fetch("http://localhost:8080/todos", {
+        method: "POST",
+        body: JSON.stringify(snakelize(newTodo)),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      let json = await response.json();
 
-      newTodo.id = this.todos.length > 0 ? ++maxId : 1;
+      let todo = new TodoItem();
+      
+      todo.isDone = camelize(json).isDone;
+      todo.name = json.name;
 
       event.target.reset();
-      this.todos.push(newTodo);
+      this.todos.push(todo);
       this.renderTodos();
       saveTodos(this.todos);
     });
