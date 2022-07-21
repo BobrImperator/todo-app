53d1f77879d88439270367e8a3d855685d44fd67
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2022-07-21 20:33:57 +0200

    React 2 (#11)
    * chore: create react-todo app
    
    * feat: simple todo app in react
    
    * chore: setup new app, copy the previous one
    
    * chore: setup linting
    
    * chore: add recommended extensions

---
+++ issue-board/.eslintrc.js
@@ -0,0 +1,47 @@
+"use strict";
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    "jest/globals": true,
+  },
+  plugins: ["@typescript-eslint/eslint-plugin", "react", "prettier", "jest"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "jest/no-hooks": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "jsx-runtime": {
+      plugins: ["react"],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      rules: {
+        "react/react-in-jsx-scope": 0,
+        "react/jsx-uses-react": 0,
+      },
+    },
+  },
+};
